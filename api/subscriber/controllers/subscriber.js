ceff99a0b8a40937e798a371563976ed7592e312
Jose Navas <jesquivelnavas@gmail.com>
2021-07-04 21:23:21 -0600

    Integrates payment and subscribe

--- api/subscriber/controllers/subscriber.js
+++ api/subscriber/controllers/subscriber.js
@@ -1,8 +1,81 @@
 'use strict'
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils')
+const { v4 } = require('uuid')
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const r2co = require('../../../lib/2checkout')
 
-module.exports = {}
+module.exports = {
+  async create(ctx) {
+    let entity
+    let data
+    let files
+
+    if (ctx.is('multipart')) {
+      parsedData = parseMultipartData(ctx)
+      data = parsedData.data
+      files = parsedData.files
+    } else {
+      data = ctx.request.body
+    }
+
+    const res = await r2co({
+      url: '/orders',
+      method: 'POST',
+      data: {
+        Country: data.state.country.code,
+        Currency: 'GTQ',
+        CustomerIP: ctx.request.ip,
+        Language: 'es',
+        Source: ctx.request.hostname,
+        Items: [
+          {
+            Code: data.productCode,
+            Quantity: 1,
+          },
+        ],
+        BillingDetails: {
+          FirstName: data.firstName,
+          LastName: data.lastName,
+          CountryCode: 'GT',
+          State: data.state.name,
+          City: data.city,
+          Address1: data.address,
+          Zip: data.zip,
+          Email: data.email,
+        },
+        PaymentDetails: {
+          Type: 'TEST',
+          Currency: 'GTQ',
+          PaymentMethod: {
+            EesToken: data.eesToken,
+            Vendor3DSReturnURL: ctx.request.hostname,
+            Vendor3DSCancelURL: ctx.request.hostname,
+          },
+          Type: 'TEST',
+        },
+      },
+    }).catch((err) => {
+      console.error(err)
+    })
+
+    if (!res || res.status !== 201 || res.data.Errors)
+      throw strapi.errors.badRequest(
+        'No se pudo completar el pago, intentalo más tarde'
+      )
+
+    data.uuid = v4()
+    data.phone = [{ number: data.phone }, { number: data.phone2 }]
+    data.address = [
+      {
+        state: data.state.id,
+        country: data.state.country.id,
+        address: `${data.address}, ${data.city}`,
+      },
+    ]
+
+    if (files) entity = await strapi.services.subscriber.create(data, { files })
+    else entity = await strapi.services.subscriber.create(data)
+
+    return sanitizeEntity(entity, { model: strapi.models.subscriber })
+  },
+}
--- api/subscriber/models/subscriber.js
+++ api/subscriber/models/subscriber.js
@@ -1,59 +1,8 @@
 'use strict'
-const { v4 } = require('uuid')
-const r2co = require('../../../lib/2checkout')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {
-  lifecycles: {
-    beforeCreate: async (data) => {
-      const res = await r2co({
-        url: '/orders',
-        method: 'POST',
-        data: {
-          Country: 'GT',
-          Currency: 'GTQ',
-          CustomerIP: '10.10.10.10', //Shopper IP
-          Language: 'es',
-          Source: 'website host',
-          Items: [
-            {
-              Code: 'UEZ4SB85SD',
-              Quantity: 1,
-            },
-          ],
-          BillingDetails: {
-            FirstName: 'Customer First Name',
-            LastName: 'Customer Last Name',
-            CountryCode: 'US',
-            State: 'California',
-            City: 'San Francisco',
-            Address1: 'Example Street',
-            Zip: '90210',
-            Email: 'example@email.com',
-          },
-          PaymentDetails: {
-            Type: 'TEST',
-            Currency: 'GTQ',
-            PaymentMethod: {
-              EesToken: data.eesToken,
-              Vendor3DSReturnURL: 'www.site.com',
-              Vendor3DSCancelURL: 'www.site.com',
-            },
-            Type: 'TEST',
-          },
-        },
-      })
-
-      console.log(res)
-
-      throw strapi.errors.badRequest(
-        'Some message you want to show in the admin UI'
-      )
-      data.uuid = v4()
-    },
-  },
-}
+module.exports = {}
